b7d94c206002b167d9f492fd59af1407f5090def
amogh <amogh.s.phadke@gmail.com>
2018-06-27 18:30:09 -0600

    Comitting code for TODO app

---
+++ todo-services/src/main/java/com/asp/todoapp/repositories/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.asp.todoapp.repositories;
+
+import com.asp.todoapp.models.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+}
